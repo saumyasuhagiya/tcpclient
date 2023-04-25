6dbf139e1cf961c3434225bd4c6174d73704dd25
Saumya Suhagiya <saumya.patel2001@gmail.com>
2023-04-25 11:11:22 -0500

    Initial code for spring integration TCP client

---
+++ src/main/java/com/example/tcpclient/tcpclient/service/TcpMessageSender.java
@@ -0,0 +1,34 @@
+package com.example.tcpclient.tcpclient.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.ip.tcp.TcpSendingMessageHandler;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.stereotype.Component;
+
+@EnableIntegration
+@Component
+public class TcpMessageSender implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    TcpSendingMessageHandler tcpSendingMessageHandler;
+    private final MessageChannel tcpClientChannel;
+
+    public TcpMessageSender(@Qualifier("messageChannel") MessageChannel messageChannel) {
+        this.tcpClientChannel = messageChannel;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        String messagePayload = "Hello, TCP server!";
+        Message<byte[]> message = MessageBuilder.withPayload(messagePayload.getBytes()).build();
+        tcpClientChannel.send(message);
+        System.out.println("Message sent to TCP server: " + messagePayload);
+        tcpSendingMessageHandler.handleMessage(message);
+    }
+}
