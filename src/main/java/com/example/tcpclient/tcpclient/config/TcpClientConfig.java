c70f670de865e94e7962024bb4a41ca37d9f6edf
Saumya Suhagiya <saumya.patel2001@gmail.com>
2023-04-26 13:30:29 -0500

    Removed commented code snippets

--- src/main/java/com/example/tcpclient/tcpclient/config/TcpClientConfig.java
+++ src/main/java/com/example/tcpclient/tcpclient/config/TcpClientConfig.java
@@ -1,22 +1,14 @@
 package com.example.tcpclient.tcpclient.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.event.EventListener;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.QueueChannel;
-import org.springframework.integration.event.inbound.ApplicationEventListeningMessageProducer;
 import org.springframework.integration.ip.tcp.TcpReceivingChannelAdapter;
 import org.springframework.integration.ip.tcp.TcpSendingMessageHandler;
 import org.springframework.integration.ip.tcp.connection.AbstractClientConnectionFactory;
-import org.springframework.integration.ip.tcp.connection.TcpConnectionEvent;
 import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
 import org.springframework.scheduling.support.PeriodicTrigger;
 
@@ -31,11 +23,6 @@ public class TcpClientConfig {
   public PollableChannel responseChannel() {
     return new QueueChannel();
   }
-
-  /* @Bean
-  public MessageChannel messageChannel() {
-    return new DirectChannel();
-  } */
 
   @Bean(name = PollerMetadata.DEFAULT_POLLER)
   public PollerMetadata defaultPoller() {
@@ -67,22 +54,4 @@ public class TcpClientConfig {
     tcpSendingMessageHandler.handleMessage(MessageBuilder.withPayload("payload").build());
     return tcpSendingMessageHandler;
   }
-
-/*   @EventListener
-    public void listen(TcpConnectionEvent event) {
-        
-        System.out.println("Event" + event);
-    }
-
-    @Bean
-public ApplicationEventListeningMessageProducer eventsAdapter(
-            MessageChannel eventChannel, MessageChannel eventErrorChannel) {
-
-    ApplicationEventListeningMessageProducer producer =
-        new ApplicationEventListeningMessageProducer();
-    producer.setEventTypes(TcpConnectionEvent.class);
-    producer.setOutputChannel(eventChannel);
-    producer.setErrorChannel(eventErrorChannel);
-    return producer;
-} */
 }
