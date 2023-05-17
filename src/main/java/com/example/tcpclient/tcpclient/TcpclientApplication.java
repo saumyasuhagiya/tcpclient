2cdca957b42eae70ea8ccce2049412e7c325d9b1
Saumya Suhagiya <saumya.patel2001@gmail.com>
2023-05-17 19:34:06 -0400

    Initial code for spring integration TCP client

---
+++ src/main/java/com/example/tcpclient/tcpclient/TcpclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcpclient.tcpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcpclientApplication.class, args);
+	}
+
+}
