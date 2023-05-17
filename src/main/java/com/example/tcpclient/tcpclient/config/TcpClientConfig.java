c47a8f027e78ee1fe10ebe6d37ec5651ad757f88
Saumya Suhagiya <saumya.patel2001@gmail.com>
2023-05-17 19:34:51 -0400

    Added event support to capture TcpEvents

--- src/main/java/com/example/tcpclient/tcpclient/config/TcpClientConfig.java
+++ src/main/java/com/example/tcpclient/tcpclient/config/TcpClientConfig.java
@@ -3,12 +3,15 @@ package com.example.tcpclient.tcpclient.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.event.inbound.ApplicationEventListeningMessageProducer;
 import org.springframework.integration.ip.tcp.TcpReceivingChannelAdapter;
 import org.springframework.integration.ip.tcp.TcpSendingMessageHandler;
 import org.springframework.integration.ip.tcp.connection.AbstractClientConnectionFactory;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionEvent;
 import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
@@ -38,6 +41,7 @@ public class TcpClientConfig {
     TcpReceivingChannelAdapter adapter = new TcpReceivingChannelAdapter();
     adapter.setConnectionFactory(connectionFactory);
     adapter.setOutputChannel(messageChannel());
+    //adapter.setClientMode(true);
     return adapter;
   }
 
@@ -49,4 +53,22 @@ public class TcpClientConfig {
     tcpSendingMessageHandler.setLoggingEnabled(true);
     return tcpSendingMessageHandler;
   }
+
+  @EventListener
+  public void connectionEvent(TcpConnectionEvent event) {
+    log.info("Event received.. {}", event);
+  }
+
+  @Bean
+  public ApplicationEventListeningMessageProducer eventsAdapter(
+          MessageChannel messageChannel, MessageChannel errorChannel) {
+
+    ApplicationEventListeningMessageProducer producer =
+            new ApplicationEventListeningMessageProducer();
+    producer.setEventTypes(TcpConnectionEvent.class);
+    producer.setOutputChannel(messageChannel);
+    producer.setErrorChannel(errorChannel);
+    return producer;
+  }
+
 }
