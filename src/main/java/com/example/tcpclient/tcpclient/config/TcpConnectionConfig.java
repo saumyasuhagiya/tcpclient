387d62de3da0c35dfd463720f9fea6d637a13cc3
Saumya Suhagiya <saumya.patel2001@gmail.com>
2023-05-17 19:29:33 -0400

    Initial code for spring integration TCP client

---
+++ src/main/java/com/example/tcpclient/tcpclient/config/TcpConnectionConfig.java
@@ -0,0 +1,52 @@
+package com.example.tcpclient.tcpclient.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.ip.tcp.connection.AbstractClientConnectionFactory;
+import org.springframework.integration.ip.tcp.connection.DefaultTcpNetSocketFactorySupport;
+import org.springframework.integration.ip.tcp.connection.FailoverClientConnectionFactory;
+import org.springframework.integration.ip.tcp.connection.TcpNetClientConnectionFactory;
+import org.springframework.integration.ip.tcp.serializer.TcpCodecs;
+
+
+@Configuration
+public class TcpConnectionConfig {
+
+  //This is for testing purpose only. Actual server resides on external server.
+  private String tcpServerAddress = "localhost";
+
+  private int tcpServerPort = 8888;
+
+  @Bean
+  public AbstractClientConnectionFactory clientConnectionFactory() {
+    List<AbstractClientConnectionFactory> tcpNetClientConnectionFactoryList = new ArrayList<>();
+    /*tcpNetClientConnectionFactoryList.add(
+    getTcpNetClientConnectionFactory(tcpServerFailoverAddress, tcpServerFailoverPort));*/
+    tcpNetClientConnectionFactoryList.add(
+        getTcpNetClientConnectionFactory(tcpServerAddress, tcpServerPort));
+
+    return getFailOverClientConnection(tcpNetClientConnectionFactoryList);
+  }
+
+  private TcpNetClientConnectionFactory getTcpNetClientConnectionFactory(String address, int port) {
+    TcpNetClientConnectionFactory tcpNetClientConnectionFactory =
+        new TcpNetClientConnectionFactory(address, port);
+    tcpNetClientConnectionFactory.setTcpSocketFactorySupport(new DefaultTcpNetSocketFactorySupport());
+    tcpNetClientConnectionFactory.setSoKeepAlive(true);
+    tcpNetClientConnectionFactory.setSingleUse(false);
+    tcpNetClientConnectionFactory.setSoTcpNoDelay(true);
+    tcpNetClientConnectionFactory.setDeserializer(TcpCodecs.lengthHeader1());
+    return tcpNetClientConnectionFactory;
+  }
+
+  private static FailoverClientConnectionFactory getFailOverClientConnection(
+      List<AbstractClientConnectionFactory> tcpNetClientConnectionFactoryList) {
+    var failoverClientConnectionFactory =
+        new FailoverClientConnectionFactory(tcpNetClientConnectionFactoryList);
+    failoverClientConnectionFactory.start();
+    return failoverClientConnectionFactory;
+  }
+}
---
+++ src/test/java/com/example/tcpclient/tcpclient/TcpclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tcpclient.tcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
