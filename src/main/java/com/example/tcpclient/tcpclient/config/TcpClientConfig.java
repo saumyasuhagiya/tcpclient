12ea031a1c13a474dc47d8893757b9f606235367
Saumya Suhagiya <saumya.patel2001@gmail.com>
2023-05-17 19:35:55 -0400

    Initial code for spring integration TCP client

---
+++ src/main/java/com/example/tcpclient/tcpclient/config/TcpClientConfig.java
@@ -0,0 +1,58 @@
+package com.example.tcpclient.tcpclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.ip.tcp.TcpReceivingChannelAdapter;
+import org.springframework.integration.ip.tcp.TcpSendingMessageHandler;
+import org.springframework.integration.ip.tcp.connection.AbstractClientConnectionFactory;
+import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.scheduling.support.PeriodicTrigger;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ComponentScan
+@Configuration
+@Slf4j
+public class TcpClientConfig {
+
+  @Bean
+  public PollableChannel responseChannel() {
+    return new QueueChannel();
+  }
+
+  @Bean(name = PollerMetadata.DEFAULT_POLLER)
+  public PollerMetadata defaultPoller() {
+    PollerMetadata pollerMetadata = new PollerMetadata();
+    pollerMetadata.setTrigger(new PeriodicTrigger(0));
+    return pollerMetadata;
+  }
+
+  @Bean
+  public TcpReceivingChannelAdapter in(AbstractClientConnectionFactory connectionFactory) {
+    TcpReceivingChannelAdapter adapter = new TcpReceivingChannelAdapter();
+    adapter.setConnectionFactory(connectionFactory);
+    adapter.setOutputChannel(responseChannel());
+    return adapter;
+  }
+
+  @Bean
+  public TcpSendingMessageHandler out(AbstractClientConnectionFactory connectionFactory)
+      throws InterruptedException {
+    log.info(
+        "Connecting to : {}:{}",
+        connectionFactory.getConnection().getHostAddress(),
+        connectionFactory.getConnection().getSocketInfo().getLocalPort());
+    TcpSendingMessageHandler tcpSendingMessageHandler = new TcpSendingMessageHandler();
+    tcpSendingMessageHandler.setConnectionFactory(connectionFactory);
+    tcpSendingMessageHandler.setLoggingEnabled(true);
+    connectionFactory.registerSender(tcpSendingMessageHandler);
+    tcpSendingMessageHandler.handleMessage(
+        MessageBuilder.withPayload("initial-msg-with-auth-details")
+            .build());
+    return tcpSendingMessageHandler;
+  }
+}
---
+++ src/main/java/com/example/tcpclient/tcpclient/service/TcpDataReceiver.java
@@ -0,0 +1,29 @@
+package com.example.tcpclient.tcpclient.service;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@MessageEndpoint
+public class TcpDataReceiver {
+
+  @ServiceActivator(inputChannel = "responseChannel")
+  public void replyHandler(Message message) {
+
+    String data = new String((byte[]) message.getPayload(), StandardCharsets.UTF_8);
+
+    log.info(
+        "ConnectionId: {}, IP Address: {}, IP Hostname: {}",
+        message.getHeaders().get("ip_connectionId"),
+        message.getHeaders().get("ip_address"),
+        message.getHeaders().get("ip_hostname"));
+    log.info(data);
+  }
+}
